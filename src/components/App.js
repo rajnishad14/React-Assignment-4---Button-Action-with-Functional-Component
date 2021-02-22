6baed2a10909d0006a74fa653123d2bae5559b09
rajnishad14 <43420327+rajnishad14@users.noreply.github.com>
2021-02-22 22:58:30 +0530

    Update App.js
    added components and functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [toggler,setToggler]=useState(false);
+  const [stre,setStre]=useState(`Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`);
   return (
     <div id="main">
-      // Do not alter the main div
+      {toggler?<p id="para">{stre}</p>:""}
+			<button id="click" onClick={()=>setToggler(!toggler)}>display</button>
     </div>
   );
 }
